4b28eaeea54b7a6d8f80bbb5223a95051cbb1b51
yuechen-yang <yuechen.yang@vanderbilt.edu>
2022-02-28 11:33:35 -0600

    asd

--- calculator.py
+++ calculator.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 
 
+
 # Plot the function
 def draw_plot(x,y):
   fig = plt.figure()
@@ -15,6 +16,13 @@ def draw_plot(x,y):
   plt.grid(linestyle = '--')
   return st.pyplot(fig)
 
+def get_result(a1,a2,b1,b2,n):
+    x = sp.Symbol('x', real=True)
+    f = "x*(" + str(a2) + "-x)*((" + str(b2) + "-" + str(n) + "*x))**" + str(n) + "-x*(" + str(a1) + "-x)*((" + str(
+        b1) + "-" + str(n) + "*x))**" + str(n) + ""
+    result = sp.solveset(f, domain=sp.S.Reals)
+    return result
+
 header= st.container()
 data=st.container()
 
@@ -23,15 +31,23 @@ with header:
 
 with data:
     st.subheader('输入数据')
-    x=st.selectbox('未知数:',options=['x'])
-    input_fun=st.text_input('方程:','2*x^3+2*x^2+x-5/10')
+    #x=st.selectbox('未知数:',options=['x'])
+    lef,rig=st.columns(2)
+    a1=lef.text_input('a1:',0.5)
+    b1 = rig.text_input('b1:',0.3)
+    a2= lef.text_input('a2:',0.6)
+    b2 = rig.text_input('b2:',0.4)
+    n=st.text_input('n:',2)
+
 
     ## calculate
-    x = sp.Symbol(x,real=True)
-    f = input_fun
-    result = sp.solveset(f,domain=sp.S.Reals)
+    #x = sp.Symbol('x',real=True)
+    #f = r"x*("+str(a2)+"-x)*(("+str(b2)+"-"+str(n)+"*x))**"+str(n)+"-x*("+str(a1)+"-x)*(("+str(b1)+"-"+str(n)+"*x))**"+str(n)+""
+    #result= sp.solveset(f,domain=sp.S.Reals)
+
 
     st.subheader('x的结果:')
+    result=get_result(a1,a2,b1,b2,n)
 
     if len(result)==0:
         st.text("无实数解")
@@ -39,12 +55,13 @@ with data:
         for i in result:
             st.markdown("* "+str(i.evalf()))
 
+        input_fun = "x*(" + str(a2) + "-x)*((" + str(b2) + "-" + str(n) + "*x))**" + str(n) + "-x*(" + str(a1) + "-x)*((" + str(
+            b1) + "-" + str(n) + "*x))**" + str(n) + ""
+
 
         st.text("图像")
-        #min=st.slider("最小值:",min_value=-20,max_value=20,value=-5)
-        #max = st.slider("最大值:", min_value=min, max_value=50, value=5)
-        x = np.linspace(-3, 3, 50)
+        x = np.linspace(-1, 1, 30)
         input_fun=input_fun.replace("^","**")
         y = eval(input_fun)
-        #x = np.linspace(-3, 3, 50)
+        x = np.linspace(-1, 1, 30)
         draw_plot(x,y)
